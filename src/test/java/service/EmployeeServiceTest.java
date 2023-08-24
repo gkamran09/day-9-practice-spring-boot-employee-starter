71b095eb51a33528b2b2d606dae342f7ce0cec32
Gerard Kamran Kalambacal <kamrangerardkalambacal@gmail.com>
2023-08-24 20:20:32 +0800

    test: added test case should_get_employee_by_id in Employee Services test

--- src/test/java/service/EmployeeServiceTest.java
+++ src/test/java/service/EmployeeServiceTest.java
@@ -1,10 +1,19 @@
 package service;
 
+import com.afs.restapi.entity.Employee;
 import com.afs.restapi.repository.EmployeeJpaRepository;
 import com.afs.restapi.service.EmployeeService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
 
 public class EmployeeServiceTest {
 
@@ -17,5 +26,22 @@ public class EmployeeServiceTest {
     void setUp() {
         MockitoAnnotations.openMocks(this);
         employeeService = new EmployeeService(employeeRepository);
+    }
+
+    @Test
+    void should_get_employee_by_id() {
+        // Given
+        Employee mockEmployee = new Employee();
+        mockEmployee.setId(1L);
+        mockEmployee.setName("John");
+        when(employeeRepository.findById(anyLong())).thenReturn(Optional.of(mockEmployee));
+
+        // When
+        Employee retrievedEmployee = employeeService.findById(1L);
+
+        // Then
+        assertNotNull(retrievedEmployee);
+        assertEquals("John", retrievedEmployee.getName());
+        assertEquals(1L, retrievedEmployee.getId());
     }
 }
