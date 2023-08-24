e3945c9150e31198fe82e41e3e7bd9f02ff70149
Gerard Kamran Kalambacal <kamrangerardkalambacal@gmail.com>
2023-08-24 15:48:16 +0800

    test: modified should_find_employee_by_gender

--- src/test/java/com/afs/restapi/EmployeeApiTest.java
+++ src/test/java/com/afs/restapi/EmployeeApiTest.java
@@ -123,12 +123,12 @@ class EmployeeApiTest {
     @Test
     void should_find_employee_by_gender() throws Exception {
         Employee employee = getEmployeeBob();
-        inMemoryEmployeeRepository.insert(employee);
+        Employee saveEmployee = employeeJpaRepository.save(employee);
 
         mockMvc.perform(get("/employees?gender={0}", "Male"))
                 .andExpect(MockMvcResultMatchers.status().is(200))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(1))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").value(1L))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").value(saveEmployee.getId()))
                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value(employee.getName()))
                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].age").value(employee.getAge()))
                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].gender").value(employee.getGender()))
